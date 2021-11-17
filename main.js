c6cc607c3a235a4be6ec6b72e34ccc63b64fd75b
YoonBeen <84373994+YoonBeen@users.noreply.github.com>
2021-11-17 17:37:36 +0900

    11-17

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
-    if(window.scrollY > navbarHeight){
+    if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
@@ -26,39 +26,74 @@ navbarMenu.addEventListener("click", (event) => {
 
 //contact me 버튼 이동하기
 const contactMeBtn = document.querySelector(".home__contact");
-contactMeBtn.addEventListener("click", ()=> {
+contactMeBtn.addEventListener("click", () => {
     scrollIntoView("#contact");
 });
 
 //home 투명도 조절
 const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener("scroll", ()=>{
+document.addEventListener("scroll", () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
 //햄버거 메뉴 클릭할 때
 const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
-navbarToggleBtn.addEventListener("click", ()=>{
+navbarToggleBtn.addEventListener("click", () => {
     navbarMenu.classList.toggle("open");
 });
 
 //arrow-up
 const homeMainHeight = document.querySelector("#home").getBoundingClientRect().height;
 const arrowUp = document.querySelector(".arrow-up");
-document.addEventListener("scroll", ()=>{
-    if(window.scrollY/homeMainHeight > 0.5){
+document.addEventListener("scroll", () => {
+    if (window.scrollY / homeMainHeight > 0.5) {
         arrowUp.classList.add("visible");
     }
-    else{
+    else {
         arrowUp.classList.remove("visible");
     }
 });
-arrowUp.addEventListener("click", ()=>{
+arrowUp.addEventListener("click", () => {
     scrollIntoView("#home");
+});
+
+// 프로젝트 js 부분
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if (filter == null) {
+        return;
+    }
+    projects.forEach((project) => {
+        if (filter === "*" || filter === project.dataset.type) {
+            project.classList.remove("invisible");
+        } else {
+            project.classList.add("invisible");
+        }
+    });
+    const active = document.querySelector(".category__btn.selected");
+    if (active != null) {
+        active.classList.remove("selected");
+    }
+    e.target.classList.add("selected");
+    projectContainer.classList.add("anim-out");
+    setTimeout(() => {
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if (filter === "*" || filter === project.dataset.type) {
+                project.classList.remove("invisible");
+            } else {
+                project.classList.add("invisible");
+            }
+        });
+        projectContainer.classList.remove("anim-out");
+    }, 300);
 });
 
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: "smooth" });
-    }
+}
